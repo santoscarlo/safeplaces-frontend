d335808cd3db4a54b1ce45c55fb29f843e6f60be
David Christian Jones <david@dcj.codes>
2020-07-24 13:29:21 -0400

    it's a start

--- src/views/Settings/Members/index.js
+++ src/views/Settings/Members/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { container, title } from './styles.module.scss';
-import { TextInput, Select } from '@wfp/ui';
+import { container, title, comboControl } from './styles.module.scss';
+import { TextInput } from '@wfp/ui';
 import emailValidator from '../../../helpers/emailValidator';
+import Select from 'components/_shared/Select/Select';
 
 const Members = () => {
   const [email, setEmail] = useState('');
@@ -16,17 +17,31 @@ const Members = () => {
   return (
     <div className={container}>
       <h3 className={title}>Add new member</h3>
-      <TextInput
-        id="email-input"
-        hideLabel
-        labelText={null}
-        onChange={onEmail}
-        autoCorrect="off"
-        autoCapitalize="off"
-        name="email"
-        invalid={email.length && !isValidEmail}
-        invalidText={'Please enter a valid email'}
-      />
+      <div className={comboControl}>
+        <TextInput
+          id="email-input"
+          hideLabel
+          labelText={null}
+          onChange={onEmail}
+          autoCorrect="off"
+          autoCapitalize="off"
+          name="email"
+          invalid={email.length && !isValidEmail}
+          invalidText={'Please enter a valid email'}
+        />
+        <Select
+          options={[
+            {
+              value: 'tracer',
+              label: 'Contact Tracer',
+            },
+            {
+              value: 'admin',
+              label: 'Administrator',
+            },
+          ]}
+        />
+      </div>
       <h3 className={title}>Existing members</h3>
     </div>
   );
